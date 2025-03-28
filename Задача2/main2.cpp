f46829140a86940808b62439c230e370ec2fab45
vivasuk683 <vivas@DESKTOP-BE431TS>
2025-03-28 20:22:40 +0300

    доделанная 2задача

--- Задача2/main2.cpp
+++ Задача2/main2.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<map>
 #include<sstream>
+#include<fstream>
 
 void push_list(std::map<std::string, std::vector<tm>>& list) {
 	std::string answer;
@@ -39,8 +40,55 @@ void print_list(std::map<std::string, std::vector<tm>>& list) {
 	}
 }
 
+void check_birth(std::map<std::string, std::vector<tm>>& list) {
+	time_t temp = time(nullptr);
+	tm real_time;
+	tm temp_1;
+	std::vector<std::string> surname_temp_1;
+	localtime_s(&real_time, &temp);
+	temp_1 = real_time;
+	std::map<std::string, std::vector<tm>>::iterator it = list.begin();
+	bool chek = false;
+	for (it; it != list.end(); it++) {
+		for (int i = 0; i < it->second.size(); i++) {
+			if (it->second[i].tm_mon == real_time.tm_mon && it->second[i].tm_mday == real_time.tm_mday) {
+				std::cout << "Today " << it->first << " Birthday : " << std::put_time(&it->second[i], "%d/%m") << "\n";
+				continue;
+			}
+			if (it->second[i].tm_mon < real_time.tm_mon || 
+					(it->second[i].tm_mon == real_time.tm_mon && it->second[i].tm_mday < real_time.tm_mday)) {
+				continue;
+			}
+			else {
+				if (chek == false) {
+					temp_1 = it->second[i];
+					surname_temp_1.push_back(it->first);
+					chek = true;
+					continue;
+				}
+				if (it->second[i].tm_mon > temp_1.tm_mon || 
+						(it->second[i].tm_mon == temp_1.tm_mon && it->second[i].tm_mday > temp_1.tm_mday)) {
+					continue;
+				}
+				else if (it->second[i].tm_mon == temp_1.tm_mon && it->second[i].tm_mday == temp_1.tm_mday) {
+					surname_temp_1.push_back(it->first);
+				}
+				else {
+					surname_temp_1.clear();
+					surname_temp_1.push_back(it->first);
+					temp_1 = it->second[i];
+				}
+			}
+		}
+	}
+	for (int i = 0; i < surname_temp_1.size(); i++) {
+		std::cout << "Next birthday is " << surname_temp_1[i] << " : " << std::put_time(&temp_1, "%d/%m") << "\n";
+	}
+}
+
 int main() {
 	std::map<std::string, std::vector<tm>> list;
 	push_list(list);
 	print_list(list);
+	check_birth(list);
 }
